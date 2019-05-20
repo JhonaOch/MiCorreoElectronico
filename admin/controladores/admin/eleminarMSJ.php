ec2735b554867826c805f2a47aa62875db01d114
JonnathanOchoa <UPSJhona@users.noreply.github.com>
2019-05-19 19:40:05 -0500

    Eliminar msm

--- admin/controladores/admin/eleminarMSJ.php
+++ admin/controladores/admin/eleminarMSJ.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['isLogin'])) {
-    header("");
+    header("Location: ../../../public/vista/login.php");
 } elseif ($_SESSION['rol'] == 'user') {
     header("Location: ../usuario/index.php");
 }
@@ -11,7 +11,7 @@ include '../../../config/conexionBD.php';
 $cod = isset($_GET["usu_cod"]) ? trim($_GET["usu_cod"]) : null;
 $sql = "DELETE FROM mensaje WHERE mail_codigo='$cod';";
 if ($conn->query($sql) == true) {
-    echo "Eliminado";
+    echo "Mesaje eliminado";
     header("Location: ../../vista/admin/index.php");
 }
 $conn->close();
