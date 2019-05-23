50e74670bea9911c2e2166953b1f32f63d321572
JonnathanOchoa <UPSJhona@users.noreply.github.com>
2019-05-23 17:50:25 -0500

    redirigir a la pagina

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+if (!isset($_SESSION['isLogin'])) {
+    header("Location: ../../../public/vista/login.php");
+} elseif ($_SESSION['rol'] == 'admin') {
+    header("Location: ../admin/index.php");
+}
+header("Location: public/vista/login.php");
